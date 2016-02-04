bbcfd4997f35de746c3441ddcffc7295cd60ba3d
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2016-02-04 20:15:38 +0000

    Working on Incrementing Score

---
+++ FizzBuzz/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  FizzBuzz
+//
+//  Created by Robert Bowers on 04/02/2016.
+//  Copyright © 2016 Robert Bowers. All rights reserved.
+//
+
+import Foundation
+
+class Game: NSObject {
+    var score: Int
+    
+    override init() {
+        score = 0
+        super.init()
+    }
+}
---
+++ FizzBuzzTests/GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  GameTests.swift
+//  FizzBuzz
+//
+//  Created by Robert Bowers on 04/02/2016.
+//  Copyright © 2016 Robert Bowers. All rights reserved.
+//
+
+import XCTest
+
+@testable import FizzBuzz
+
+class GameTests: XCTestCase {
+    
+    let game = Game()
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testGameStartsAtZero() {
+        XCTAssertTrue(game.score == 0)
+    }
+    
+    func testOnPlayScoreIncremented() {
+        game.play()
+        XCTAssertTrue(game.score ==1)
+    }
+
+}
