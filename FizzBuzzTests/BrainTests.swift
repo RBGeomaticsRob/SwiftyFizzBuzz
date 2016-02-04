f20ce393b0ddc8b53ce371a7f9691329adb6349c
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2016-02-04 19:05:56 +0000

    Test for Div by Three

---
+++ FizzBuzzTests/BrainTests.swift
@@ -0,0 +1,29 @@
+//
+//  BrainTests.swift
+//  FizzBuzz
+//
+//  Created by Robert Bowers on 04/02/2016.
+//  Copyright © 2016 Robert Bowers. All rights reserved.
+//
+
+import XCTest
+
+class BrainTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        let result = brain.isDivisiblebyThree(3)
+        XCTAssertEqual(result, true)
+    }
+
+}
