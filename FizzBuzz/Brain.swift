f20b2c4a976631b5391338d99c1835114b699f84
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2016-02-04 19:51:28 +0000

    Refactored div methods

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -9,11 +9,9 @@
 import Foundation
 
 class Brain: NSObject {
-    func isDivisibleByThree(number: Int) -> Bool {
-        return number % 3 == 0
+    
+    func isDivisibleBy(divisor: Int, number: Int)-> Bool {
+        return number % divisor == 0
     }
     
-    func isDivisibleByFive(number: Int) -> Bool {
-        return number % 5 == 0
-    }
 }
